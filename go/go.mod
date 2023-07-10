16f439faaa82a6b6badd871b44bed3ec4e113ec5
leolxliu <leolxliu@tencent.com>
2023-07-10 20:01:35 +0800

    fix rtmp push

--- go/go.mod
+++ go/go.mod
@@ -3,6 +3,7 @@ module github.com/Tencent-RTC/rtsp-to-trtc/go
 go 1.19
 
 require (
+	github.com/3d0c/gmf v0.0.0-20220906170454-be727bc5b56c // indirect
 	github.com/asticode/go-astiav v0.10.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/tencentyun/tls-sig-api-v2-golang v1.3.0 // indirect
--- go/main.go
+++ go/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
-	"strings"
 
-	"github.com/tencentyun/tls-sig-api-v2-golang/tencentyun"
-
-	"github.com/asticode/go-astiav"
+	"github.com/3d0c/gmf"
 	"github.com/joho/godotenv"
+	"github.com/tencentyun/tls-sig-api-v2-golang/tencentyun"
 )
 
 func main() {
@@ -36,137 +33,78 @@ func main() {
 		fmt.Println(usersig)
 	}
 
-	output_url := fmt.Sprintf("rtmp://intl-rtmp.rtc.qq.com/push/%s?userid=%s&sdkappid=%d&usersig=%s", room_id, user_id, sdkappid, usersig)
+	output_url := fmt.Sprintf("rtmp://rtmp.rtc.qq.com/push/%s?userid=%s&sdkappid=%d&usersig=%s", room_id, user_id, sdkappid, usersig)
 
 	fmt.Println(output_url)
 
-	astiav.SetLogLevel(astiav.LogLevelDebug)
-	astiav.SetLogCallback(func(l astiav.LogLevel, fmt, msg, parent string) {
-		log.Printf("ffmpeg log: %s (level: %d)\n", strings.TrimSpace(msg), l)
-	})
+	gmf.LogSetLevel(gmf.AV_LOG_DEBUG)
 
-	pkt := astiav.AllocPacket()
-	defer pkt.Free()
-
-	inputFormatContext := astiav.AllocFormatContext()
-	if inputFormatContext == nil {
-		log.Fatal(errors.New("main: input format context is nil"))
-	}
-	defer inputFormatContext.Free()
-
-	if err := inputFormatContext.OpenInput(rtsp_url, nil, nil); err != nil {
-		log.Fatal(fmt.Errorf("main: opening input failed: %w", err))
-	}
-	defer inputFormatContext.CloseInput()
-
-	// Find stream info
-	if err := inputFormatContext.FindStreamInfo(nil); err != nil {
-		log.Fatal(fmt.Errorf("main: finding stream info failed: %w", err))
-	}
-
-	// Alloc output format context
-	outputFormatContext, err := astiav.AllocOutputFormatContext(nil, "flv", "aa.flv")
+	inputCtx, err := gmf.NewInputCtx(rtsp_url)
 	if err != nil {
-		log.Fatal(fmt.Errorf("main: allocating output format context failed: %w", err))
+		fmt.Println(err.Error())
+		return
 	}
-	if outputFormatContext == nil {
-		log.Fatal(errors.New("main: output format context is nil"))
-	}
-	defer outputFormatContext.Free()
 
-	inputStreams := make(map[int]*astiav.Stream)
-	outputStreams := make(map[int]*astiav.Stream)
-	for _, istream := range inputFormatContext.Streams() {
-		// Only process audio or video
-		if istream.CodecParameters().MediaType() != astiav.MediaTypeAudio &&
-			istream.CodecParameters().MediaType() != astiav.MediaTypeVideo {
+	defer inputCtx.CloseInput()
+
+	inputCtx.Dump()
+
+	outputCtx, err := gmf.NewOutputCtxWithFormatName(output_url, "flv")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer outputCtx.CloseOutput()
+
+	fmt.Println("===================================")
+
+	for i := 0; i < inputCtx.StreamsCnt(); i++ {
+		srcStream, err := inputCtx.GetStream(i)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		_, err = outputCtx.AddStreamWithCodeCtx(srcStream.CodecCtx())
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	}
+
+	outputCtx.Dump()
+
+	if err := outputCtx.WriteHeader(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// first := false
+	for packet := range inputCtx.GetNewPackets() {
+
+		fmt.Println("===================================", packet.Pts(), packet.StreamIndex())
+
+		if packet.Dts() < 0 {
 			continue
 		}
 
-		// Add input stream
-		inputStreams[istream.Index()] = istream
-
-		if istream.CodecParameters().MediaType() == astiav.MediaTypeAudio {
-			fmt.Println("index ", istream.Index(), " audio")
+		ost, err := outputCtx.GetStream(packet.StreamIndex())
+		if err != nil {
+			fmt.Println(err.Error())
 		}
 
-		if istream.CodecParameters().MediaType() == astiav.MediaTypeVideo {
-			fmt.Println("index ", istream.Index(), " video")
+		gmf.RescaleTs(packet, ost.CodecCtx().TimeBase(), ost.TimeBase())
+
+		//if first { //if read from rtsp ,the first packets is wrong.
+		if err := outputCtx.WritePacket(packet); err != nil {
+			fmt.Println(err.Error())
+			return
 		}
+		// //}
 
-		// Add stream to output format context
-		ostream := outputFormatContext.NewStream(nil)
-		if ostream == nil {
-			log.Fatal(errors.New("main: output stream is nil"))
-		}
-
-		// Copy codec parameters
-		if err = istream.CodecParameters().Copy(ostream.CodecParameters()); err != nil {
-			log.Fatal(fmt.Errorf("main: copying codec parameters failed: %w", err))
-		}
-
-		// Reset codec tag
-		ostream.CodecParameters().SetCodecTag(0)
-
-		// Add output stream
-		outputStreams[istream.Index()] = ostream
-	}
-
-	if err = outputFormatContext.WriteHeader(nil); err != nil {
-		log.Fatal(fmt.Errorf("main: writing header failed: %w", err))
-	}
-
-	got_first_key_frame := false
-
-	// Loop through packets
-	for {
-		// Read frame
-		if err = inputFormatContext.ReadFrame(pkt); err != nil {
-			if errors.Is(err, astiav.ErrEof) {
-				break
-			}
-			log.Fatal(fmt.Errorf("main: reading frame failed: %w", err))
-		}
-
-		// wait fot keyframe to start
-		if !got_first_key_frame {
-			if !pkt.Flags().Has(astiav.PacketFlagKey) || pkt.StreamIndex() == 0 {
-				pkt.Unref()
-				continue
-			}
-			got_first_key_frame = true
-		}
-
-		fmt.Println("pkt: ", pkt.StreamIndex(), pkt.Flags().Has(astiav.PacketFlagKey))
-
-		//Get input stream
-		inputStream, ok := inputStreams[pkt.StreamIndex()]
-		if !ok {
-			pkt.Unref()
-			continue
-		}
-
-		// Get output stream
-		outputStream, ok := outputStreams[pkt.StreamIndex()]
-		if !ok {
-			pkt.Unref()
-			continue
-		}
-
-		// Update packet
-		pkt.SetStreamIndex(outputStream.Index())
-		pkt.RescaleTs(inputStream.TimeBase(), outputStream.TimeBase())
-		pkt.SetPos(-1)
-
-		// Write frame
-		if err = outputFormatContext.WriteInterleavedFrame(pkt); err != nil {
-			log.Fatal(fmt.Errorf("main: writing interleaved frame failed: %w", err))
-		}
-	}
-
-	// Write trailer
-	if err = outputFormatContext.WriteTrailer(); err != nil {
-		log.Fatal(fmt.Errorf("main: writing trailer failed: %w", err))
+		// // first = true
+		packet.Free()
 	}
 
 }
